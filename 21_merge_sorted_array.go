c25a8e528c3342603a53776e449f2da6206ba52b
Thai Nguyen <nguyenquocthai0407@gmail.com>
2024-03-31 19:29:35 +0700

    add leetcode

--- 21_merge_sorted_array.go
+++ 21_merge_sorted_array.go
@@ -27,10 +27,29 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 	}
 }
 
+func merge2(nums1 []int, m int, nums2 []int, n int) {
+	i := m - 1
+	j := n - 1
+	k := m + n - 1
+
+	for ; k >= 0; k-- {
+		if j < 0 {
+			return
+		}
+		if nums2[j] > nums1[i] {
+			nums1[k] = nums2[j]
+			j--
+		} else {
+			nums1[k] = nums1[i]
+			i--
+		}
+	}
+}
+
 func main() {
 	nums1 := []int{1, 2, 3, 0, 0, 0}
 	nums2 := []int{2, 5, 6}
 
-	merge(nums1, 3, nums2, 3)
+	merge2(nums1, 3, nums2, 3)
 	fmt.Sprintln(nums1)
 }
